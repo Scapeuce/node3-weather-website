491694ea763efbaa769b8cc4857b1776a0f85b09
Andres <andres.lillo.p@gmail.com>
2020-01-07 19:00:52 +1300

    Commented unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-console.log("Client javascript file loaded")
+// console.log("Client javascript file loaded")
 // fetch('http://puzzle.mead.io/puzzle').then((response) => {
 //     response.json().then((data) => {
 //         console.log(data)
